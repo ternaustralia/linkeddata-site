34041ed6d43591a41236dc3eecdf25720a2553e2
Edmond Chuc <edmond.chuc@outlook.com>
2021-10-11 01:32:39 +1000

    Local dev data fetching working using SWR with urlencoded form data.

--- src/pages/viewers/tern-ontology.js
+++ src/pages/viewers/tern-ontology.js
@@ -5,64 +5,94 @@ import {
   Link,
   useLocation
 } from "react-router-dom";
+import useSWR from 'swr';
+// import { fetcher } from './_dataFetcher';
+// const fetcher = (...args) => fetch(...args).then(res=> res.json())
+const fetcher = async (...args) => {
+  const url = args[0]
+  const fetchOptions = JSON.parse(args[1])
+  const res = await fetch(url, fetchOptions)
 
-// A custom hook that builds on useLocation to parse
-// the query string for you.
+  if(!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.info = await res.json()
+    error.status = res.status
+    throw error
+  }
+
+  return res.json()
+}
+
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-function Child({ name }) {
-  return (
-    <div>
-      {name ? (
-        <h3>
-          The <code>name</code> in the query string is &quot;{name}
-          &quot;
-        </h3>
-      ) : (
-        <h3>There is no name in the query string</h3>
-      )}
-    </div>
-  );
+function getSparqlQuery(classUri) {
+  return `
+PREFIX tern: <https://w3id.org/tern/ontologies/tern/>
+select *
+where {
+    <${classUri}> ?p ?o .
+}
+  `
 }
 
 function PageComponent() {
   let query = useQuery();
   const pageRoute = '/viewers/tern-ontology'
-  const uri = query.get('uri')
-  const endpoint = query.get('endpoint')
+  const uri = query.get('uri') || 'https://w3id.org/tern/ontologies/tern/Observation'
+  const endpoint = query.get('endpoint') || 'http://localhost:7200/repositories/ontologies'
+  
+  const sparqlQuery = getSparqlQuery(uri)
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      accept: 'application/sparql-results+json',
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    // https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
+    body: [encodeURIComponent('query') + '=' + encodeURIComponent(getSparqlQuery(uri))]
+  }
+  
+  // Reason we serialise fetchOptions is to avoid infinite loop.
+  // See https://github.com/vercel/swr/issues/345
+  const { data, error } = useSWR([endpoint, JSON.stringify(fetchOptions)], fetcher)
+  
+  const debug = true
+
+  const debugView = debug ? <div>
+    <p>{`URI: ${uri}`}</p>
+    <p>{`SPARQL endpoint: ${endpoint}`}</p>
+    <p></p>
+    <pre>{sparqlQuery}</pre>
+    <p>Received: </p>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </div> : ''
+
+  if(error) return <div>Failed to load</div>
+  if(!data) return <div>Loading...</div>
 
   return (
-    <>
+    <main className="container container--fluid margin-vert--lg">
       <ul>
         <li>
-          <Link to={`${pageRoute}?uri=https://w3id.org/tern/ontologies/tern/Observation&endpoint=https://graphdb.tern.org.au/repositories/ontologies`}>Ecological Site</Link>
+          <Link to={`${pageRoute}?uri=https://w3id.org/tern/ontologies/tern/Observation&endpoint=http://localhost:7200/repositories/ontologies`}>Ecological Site</Link>
         </li>
         <li>
-          <Link to={`${pageRoute}?uri=https://w3id.org/tern/ontologies/tern/Transect&endpoint=https://graphdb.tern.org.au/repositories/ontologies`}>Transect</Link>
+          <Link to={`${pageRoute}?uri=https://w3id.org/tern/ontologies/tern/Transect&endpoint=http://localhost:7200/repositories/ontologies`}>Transect</Link>
         </li>
       </ul>
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>{`URI: ${uri}`}</p>
-        <p>{`SPARQL endpoint: ${endpoint}`}</p>
-      </div>
-    </>
+      {debugView}
+    </main>
   )
 }
 
 export default function Page() {
 
   return (
-    <Layout title="Hello">
+    <Layout title="TERN Ontology viewer">
       <Router>
         <PageComponent />
       </Router>
