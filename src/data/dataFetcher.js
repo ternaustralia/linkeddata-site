21f35021e84243ffc9586fc343f4675da3d75eda
Edmond Chuc <e.chuc@uq.edu.au>
2021-11-22 15:39:45 +1000

    Add functional regression test for requests to the prefix server using the fetcher implementation.

--- src/data/dataFetcher.js
+++ src/data/dataFetcher.js
@@ -5,30 +5,18 @@ import { getFetchOptions } from "./utils";
 // const fetcher = (...args) => fetch(...args).then(res=> res.json())
 export const fetcher = async (...args) => {
   const url = args[0];
-  // console.log(url);
 
   let fetchOptions = null;
   if (args.length > 1) {
     fetchOptions = JSON.parse(args[1]);
+    const params = new URLSearchParams();
+    params.append("query", fetchOptions.sparqlQuery);
+    const res = await axios.post(url, params, fetchOptions);
+    return res.data;
+  } else {
+    const res = await axios.post(url, null, fetchOptions);
+    return res.data;
   }
-
-  // const res = await fetch(url, fetchOptions);
-  // console.log(args)
-  const params = new URLSearchParams();
-  // console.log(decodeURI(fetchOptions.body[0].split("=").at(-1)));
-  params.append("query", fetchOptions.sparqlQuery);
-  const res = await axios.post(url, params, fetchOptions);
-
-  // if (!res.ok) {
-  //   const error = new Error("An error occurred while fetching the data.");
-  //   // Attach extra info to the error object.
-  //   error.info = await res.json();
-  //   error.status = res.status;
-  //   throw error;
-  // }
-
-  // return res.json();
-  return res.data;
 };
 
 export function useSparql(sparqlEndpoint, sparqlQuery) {
