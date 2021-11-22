6516c658efaf8de8fb392cf87048e4b03dc90298
Edmond Chuc <e.chuc@uq.edu.au>
2021-11-22 16:17:34 +1000

    Show the URI when data is loading or if it fails to load.

--- src/components/InternalLink.js
+++ src/components/InternalLink.js
@@ -1,46 +1,61 @@
-import React from 'react'
-import useSWR from "swr"
-import { fetcher } from "../data/dataFetcher"
-import { getFetchOptions } from "../data/utils"
-import { Link } from 'react-router-dom';
+import React from "react";
+import useSWR from "swr";
+import { fetcher } from "../data/dataFetcher";
+import { getFetchOptions } from "../data/utils";
+import { Link } from "react-router-dom";
 
 function getConnegFetchOptions() {
   return {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/ld+json',
+      accept: "application/ld+json",
     },
-  }
+  };
 }
 
 function LabelByConneg({ href }) {
-  const fetchOptions = getConnegFetchOptions()
+  const fetchOptions = getConnegFetchOptions();
   const swrOptions = {
     shouldRetryOnError: false,
-    revalidateOnFocus: false
-  }
-  const { data, error } = useSWR([`https://cors-anywhere.edmondchuc.com/${href}`, JSON.stringify(fetchOptions)], fetcher, swrOptions)
+    revalidateOnFocus: false,
+  };
+  const { data, error } = useSWR(
+    [
+      `https://cors-anywhere.edmondchuc.com/${href}`,
+      JSON.stringify(fetchOptions),
+    ],
+    fetcher,
+    swrOptions
+  );
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-  
+  if (error) return <div>{href}</div>;
+  if (!data) return <div>{href}</div>;
+
   // TODO: Hardcoded skos:prefLabel here.
-  const label = data?.[0]?.['http://www.w3.org/2004/02/skos/core#prefLabel']?.[0]?.['@value']
-  return <>{label}</>
+  const label =
+    data?.[0]?.["http://www.w3.org/2004/02/skos/core#prefLabel"]?.[0]?.[
+      "@value"
+    ];
+  return <>{label}</>;
 }
 
 export default function InternalLink({ resourceUri, settings }) {
-  const {pageRoute, endpoint} = settings
-  const sparqlQuery = settings.queries.getLabel(resourceUri)
-  const fetchOptions = getFetchOptions(sparqlQuery)
-  const { data, error } = useSWR([endpoint, JSON.stringify(fetchOptions)], fetcher)
+  const { pageRoute, endpoint } = settings;
+  const sparqlQuery = settings.queries.getLabel(resourceUri);
+  const fetchOptions = getFetchOptions(sparqlQuery);
+  const { data, error } = useSWR(
+    [endpoint, JSON.stringify(fetchOptions)],
+    fetcher
+  );
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>{resourceUri}</div>;
+  if (!data) return <div>{resourceUri}</div>;
 
-  const label = data.results.bindings?.[0]?.label?.value
+  const label = data.results.bindings?.[0]?.label?.value;
 
   return (
-    <Link to={`${pageRoute}?uri=${resourceUri}`}><strong>{label ? label : <LabelByConneg href={resourceUri} />}</strong></Link>
-  )
+    <Link to={`${pageRoute}?uri=${resourceUri}`}>
+      <strong>{label ? label : <LabelByConneg href={resourceUri} />}</strong>
+    </Link>
+  );
 }
