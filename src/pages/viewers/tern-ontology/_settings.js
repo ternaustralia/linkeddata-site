fc5f28838f2469c788a74dab9cb28a3832a12f30
Edmond Chuc <e.chuc@uq.edu.au>
2022-02-04 16:16:55 +1000

    Change endpoint to prod

--- src/pages/viewers/tern-ontology/_settings.js
+++ src/pages/viewers/tern-ontology/_settings.js
@@ -3,9 +3,8 @@ import * as queries from "./_queries";
 const settings = {
   title: "TERN Ontology viewer",
   pageRoute: "/viewers/tern-ontology",
-  // endpoint: 'https://graphdb.tern.org.au/repositories/knowledge_graph_core?infer=false',
   endpoint:
-    "https://cors-anywhere.edmondchuc.com/http://graphdb-test.tern.org.au/repositories/knowledge_graph_core?infer=false",
+    "https://graphdb.tern.org.au/repositories/knowledge_graph_core?infer=false",
   queries: queries,
 };
 export default settings;
