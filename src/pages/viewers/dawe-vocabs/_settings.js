95d6dbc966ad39dc5123aa3d485b7efbefd9d527
junrongYu <junrong.yu@uq.edu.au>
2022-06-10 01:28:58 +0000

    fixed categorical collection and endpoint issues

--- src/pages/viewers/dawe-vocabs/_settings.js
+++ src/pages/viewers/dawe-vocabs/_settings.js
@@ -3,7 +3,7 @@ import * as queries from './_queries'
 const settings = {
     title: 'DAWE RLP Controlled Vocabularies',
     pageRoute: '/viewers/dawe-vocabs',
-    endpoint: 'http://localhost:7200/repositories/dawe_vocabs_core?infer=false',
+    endpoint: 'https://graphdb.tern.org.au/repositories/dawe_vocabs_core?infer=false',
     queries: queries
 }
 export default settings
