25a6b9c43b765e5ec712c6ff178e9ee66f4debb8
Edmond Chuc <e.chuc@uq.edu.au>
2022-12-07 00:47:06 +0000

    Rename menu item from DAWE to NRM

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -163,7 +163,7 @@ const config = {
               },
               {
                 to: "/viewers/dawe-vocabs",
-                label: "DAWE Controlled Vocabularies",
+                label: "NRM Controlled Vocabularies",
               },
             ],
           },
